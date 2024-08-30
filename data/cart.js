a4d405e92d5990841159b69abd5ce043d01e15a1
astacus777 <astacus@hotmail.co.uk>
2024-08-31 00:45:52 +0200

    Save data and generate HTL for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,17 +5,20 @@ cart = [
 	{
 		productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
 		quantity: 2,
+		deliveryOptionId: '1',
 	},
 	{
 		productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-		quantity: 45,
+		quantity: 2,
+		deliveryOptionId: '2',
 	},
 ];
 }
 
 
 function saveToStorage() {
-    localStorage.setItem('cart', JSON.stringify(cart))
+    localStorage.setItem('cart', JSON.stringify(cart));
+	
 }
 
 export function addToCart(productId) {
@@ -33,10 +36,12 @@ export function addToCart(productId) {
 		cart.push({
 			productId: productId,
 			quantity: 1,
+			deliveryOptionId: '1',
 		});
 	}
 
     saveToStorage();
+	
 }
 
 export function removeFromCart(productId) {
@@ -48,5 +53,6 @@ export function removeFromCart(productId) {
     })
 
     cart = newCart;
+	
     saveToStorage();
 }
